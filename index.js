be2e3d18ddc6450b1f8216f5515d0c7c6b720db5
shanmukha <shanmukha.krish123@gmail.com>
2021-11-29 23:24:40 +0100

    disable db

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 
 
 const logger = require('./services/startup/logging');
-require('./services/startup/db');
-require('./services/startup/routes')(app);
+// require('./services/startup/db');
+// require('./services/startup/routes')(app);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => logger.info(`Listening on port ${port}...`));
